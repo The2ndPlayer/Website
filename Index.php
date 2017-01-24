66e28c4fdd8add2ebff706e264a37257fd5f8c49
Jackson Tweet <jacksontweet@createninnovate.com>
2017-01-24 13:25:53 -0600

    Update Index.php

--- Index.php
+++ Index.php
@@ -8,10 +8,13 @@
   <meta name="author" content="Jackson Tweet">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   
-  <!-- CSS/Scripts -->
-  <?php include 'include/css-scripts.php';?>
+  <!-- Scripts -->
+  <?php include 'include/scripts.php';?>
+  
+  <!-- CSS -->
+  <?php include 'include/css.php';?>
  </head>
  <body onload="Title()">
-  
+  <!-- Nav Bar-->
  </body>
 </html>
