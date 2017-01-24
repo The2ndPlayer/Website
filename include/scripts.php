5a7bf9372012116ae3c710e71a2deacbdbbac91e
Jackson Tweet <jacksontweet@createninnovate.com>
2017-01-24 13:24:23 -0600

    Update and rename css-scripts.php to scripts.php

---
+++ include/scripts.php
@@ -0,0 +1,12 @@
+
+<!-- Scrolling title -->
+<SCRIPT LANGUAGE="JavaScript">
+<!-- Begin
+var scrl = " test ";
+function magicTitle() {
+ scrl = scrl.substring(1, scrl.length) + scrl.substring(0, 1);
+ document.title = scrl;
+ setTimeout("magicTitle()", 300);
+ }
+//  End -->
+</script>
